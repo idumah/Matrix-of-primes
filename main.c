657c6fe5f3e459c125e7fc598357d30bb4a55fab
dima <kondratev2004@gmail.com>
2023-11-27 16:17:00 +0300

    minor fixes

--- main.c
+++ main.c
@@ -22,6 +22,7 @@ void matrix_output(int m[5], FILE *fout) {
 
 // проверка на простое число
 int is_prime(int n) {
+    if (n == 2) return 1;
     for (int div = 2; div <= sqrt(n) + 1; div++) {
         if (n % div == 0) return 0;
     }
@@ -63,9 +64,9 @@ void find_suitable_primes(int s[10000], int sum) {
 }
 
 // проверяем сумму цифр чисел по горизонтали и по вертикали, если матрица недостроена - простая оценка: попадает ли сумма в интервал между минимально и максимально возможными значениями
-int check_sums(int m[5], int sum, int k) {
+int check_sums(int m[5], int sum, int line) {
 
-    if (k == 5) {
+    if (line == 5) {
         // проверим вертикали
         if (m[0] % 10 + m[1] % 10 + m[2] % 10 + m[3] % 10 + m[4] % 10 != sum) return 0;
         if ((m[0] / 10) % 10 + (m[1] / 10) % 10 + (m[2] / 10) % 10 + (m[3] / 10) % 10 + (m[4] / 10) % 10 != sum) return 0;
@@ -76,7 +77,7 @@ int check_sums(int m[5], int sum, int k) {
         if (m[0] % 10 + (m[1] / 10) % 10 + (m[2] / 100) % 10 + (m[3] / 1000) % 10 + (m[4] / 10000) % 10 != sum) return 0;
         if ((m[0] / 10000) % 10 + (m[1] / 1000) % 10 + (m[2] / 100) % 10 + (m[3] / 10) % 10 + m[4] % 10 != sum) return 0;
     }
-    if (k == 4) {
+    if (line == 4) {
         // проверим вертикали
         if (m[0] % 10 + m[1] % 10 + m[2] % 10 + m[3] % 10 + 1 <= sum > m[0] % 10 + m[1] % 10 + m[2] % 10 + m[3] % 10 + 9) return 0;
         if ((m[0] / 10) % 10 + (m[1] / 10) % 10 + (m[2] / 10) % 10 + (m[3] / 10) % 10 + 1 <= sum > (m[0] / 10) % 10 + (m[1] / 10) % 10 + (m[2] / 10) % 10 + (m[3] / 10) % 10 + 9) return 0;
@@ -87,7 +88,7 @@ int check_sums(int m[5], int sum, int k) {
         if (m[0] % 10 + (m[1] / 10) % 10 + (m[2] / 100) % 10 + (m[3] / 1000) % 10 + 1 <= sum > m[0] % 10 + (m[1] / 10) % 10 + (m[2] / 100) % 10 + (m[3] / 1000) % 10 + 9) return 0;
         if ((m[0] / 10000) % 10 + (m[1] / 1000) % 10 + (m[2] / 100) % 10 + (m[3] / 10) % 10 + 1 <= sum > (m[0] / 10000) % 10 + (m[1] / 1000) % 10 + (m[2] / 100) % 10 + (m[3] / 10) % 10 + 9) return 0;
     }
-    if (k == 3) {
+    if (line == 3) {
         // проверим вертикали
         if (m[0] % 10 + m[1] % 10 + m[2] % 10 + 1 <= sum > m[0] % 10 + m[1] % 10 + m[2] % 10 + 9 * 2) return 0;
         if ((m[0] / 10) % 10 + (m[1] / 10) % 10 + (m[2] / 10) % 10 + 1 <= sum > (m[0] / 10) % 10 + (m[1] / 10) % 10 + (m[2] / 10) % 10+ 9 * 2) return 0;
@@ -98,7 +99,7 @@ int check_sums(int m[5], int sum, int k) {
         if (m[0] % 10 + (m[1] / 10) % 10 + (m[2] / 100) % 10 + 1 <= sum > m[0] % 10 + (m[1] / 10) % 10 + (m[2] / 100) % 10 + 9 * 2) return 0;
         if ((m[0] / 10000) % 10 + (m[1] / 1000) % 10 + (m[2] / 100) % 10 + 1 <= sum > (m[0] / 10000) % 10 + (m[1] / 1000) % 10 + (m[2] / 100) % 10 + 9 * 2) return 0;
     }
-    if (k == 2) {
+    if (line == 2) {
         // проверим вертикали
         if (m[0] % 10 + m[1] % 10 + 1 <= sum > m[0] % 10 + m[1] % 10 + 9 * 3) return 0;
         if ((m[0] / 10) % 10 + (m[1] / 10) % 10 + 1 <= sum > (m[0] / 10) % 10 + (m[1] / 10) % 10 + 9 * 3) return 0;
@@ -109,7 +110,7 @@ int check_sums(int m[5], int sum, int k) {
         if (m[0] % 10 + (m[1] / 10) % 10 + 1 <= sum > m[0] % 10 + (m[1] / 10) % 10 + 9 * 3) return 0;
         if ((m[0] / 10000) % 10 + (m[1] / 1000) % 10 + 1 <= sum > (m[0] / 10000) % 10 + (m[1] / 1000) % 10 + 9 * 3) return 0;
     }
-    if (k == 1) {
+    if (line == 1) {
         // проверим вертикали
         if (m[0] % 10 + 1 <= sum > m[0] % 10 + 9 * 4) return 0;
         if ((m[0] / 10) % 10 + 1 <= sum > (m[0] / 10) % 10 + 9 * 4) return 0;
@@ -124,9 +125,9 @@ int check_sums(int m[5], int sum, int k) {
 }
 
 // проверяем являются ли вертикали и диагонали в матрице простыми числами, если матрица не достроена - есть ли нужные простые числа, начинающиеся с тех же цифр
-int check_primes(int m[5], int k, int primes[10000]) {
-    if (k == 0) return 1;
-    if (k == 1) {
+int check_primes(int m[5], int line, int primes[10000]) {
+    if (line == 0) return 1;
+    if (line == 1) {
         if (!cache[m[0] % 10]) return 0;
         if (!cache[(m[0] / 10) % 10]) return 0;
         if (!cache[(m[0] / 100) % 10]) return 0;
@@ -135,7 +136,7 @@ int check_primes(int m[5], int k, int primes[10000]) {
         if (!cache[(m[0] / 10000) % 10]) return 0;
         if (!prime_ends[m[0] % 10]) return 0;
         return 1; }
-    if (k == 2) {
+    if (line == 2) {
         if (!cache[m[0] % 10 * 10 + m[1] % 10]) return 0;
         if (!cache[(m[0] / 10) % 10 * 10 + (m[1] / 10) % 10]) return 0;
         if (!cache[(m[0] / 100) % 10 * 10 + (m[1] / 100) % 10]) return 0;
@@ -144,7 +145,7 @@ int check_primes(int m[5], int k, int primes[10000]) {
         if (!prime_ends[m[0] % 10 + (m[1] / 10) % 10 * 10]) return 0;
         if (!cache[(m[0] / 10000) % 10 * 10 + (m[1] / 1000) % 10]) return 0;
         return 1;}
-    if (k == 3) {
+    if (line == 3) {
         if (!cache[m[0] % 10 * 100 + m[1] % 10 * 10 + m[2] % 10]) return 0;
         if (!cache[(m[0] / 10) % 10 * 100 + (m[1] / 10) % 10 * 10 + (m[2] / 10) % 10]) return 0;
         if (!cache[(m[0] / 100) % 10 * 100 + (m[1] / 100) % 10 * 10 + (m[2] / 100) % 10]) return 0;
@@ -153,7 +154,7 @@ int check_primes(int m[5], int k, int primes[10000]) {
         if (!prime_ends[m[0] % 10 + (m[1] / 10) % 10 * 10 + (m[2] / 100) % 10 * 100]) return 0;
         if (!cache[(m[0] / 10000) % 10 * 100 + (m[1] / 1000) % 10 * 10 + (m[2] / 100) % 10]) return 0;
         return 1; }
-    if (k == 4) {
+    if (line == 4) {
         if (!cache[m[0] % 10 * 1000 + m[1] % 10 * 100 + m[2] % 10 * 10 + m[3] % 10]) return 0;
         if (!cache[(m[0] / 10) % 10 * 1000 + (m[1] / 10) % 10 * 100 + (m[2] / 10) % 10 * 10 + (m[3] / 10) % 10]) return 0;
         if (!cache[(m[0] / 100) % 10 * 1000 + (m[1] / 100) % 10 * 100 + (m[2] / 100) % 10 * 10 + (m[3] / 100) % 10]) return 0;
@@ -162,7 +163,7 @@ int check_primes(int m[5], int k, int primes[10000]) {
         if (!prime_ends[m[0] % 10 + (m[1] / 10) % 10 * 10 + (m[2] / 100) % 10 * 100 + (m[3] / 1000) % 10 * 1000]) return 0;
         if (!cache[(m[0] / 10000) % 10 * 1000 + (m[1] / 1000) % 10 * 100 + (m[2] / 100) % 10 * 10 + (m[3] / 10) % 10]) return 0;
         return 1; }
-    if (k == 5) {
+    if (line == 5) {
         if (!cache[m[0] % 10 * 10000 + m[1] % 10 * 1000 + m[2] % 10 * 100 + m[3] % 10 * 10 + m[4] % 10]) return 0;
         if (!cache[(m[0] / 10) % 10 * 10000 + (m[1] / 10) % 10 * 1000 + (m[2] / 10) % 10 * 100 + (m[3] / 10) % 10 * 10 + (m[4] / 10) % 10]) return 0;
         if (!cache[(m[0] / 100) % 10 * 10000 + (m[1] / 100) % 10 * 1000 + (m[2] / 100) % 10 * 100 + (m[3] / 100) % 10 * 10 + (m[4] / 100) % 10]) return 0;
@@ -175,18 +176,18 @@ int check_primes(int m[5], int k, int primes[10000]) {
 }
 
 // функция перебора
-void backtracking(int m[5], int k, int sum, int first, int primes[10000], FILE *fout){
-    if (check_primes(m, k, primes) && check_sums(m, sum, k)) { // проверяем матрицу на соответствие условиям задачи
-        if (k == 5) { // если матрица заполнена - выводим
+void backtracking(int m[5], int line, int sum, int first, int primes[10000], FILE *fout){
+    if (check_primes(m, line, primes) && check_sums(m, sum, line)) { // проверяем матрицу на соответствие условиям задачи
+        if (line == 5) { // если матрица заполнена - выводим
             matrix_output(m, fout);
         }
         else // иначе перебираем возможные значения незаполненных строк
             for (int i = 0; primes[i]; i++) {
-                if (k == 0 && primes[i] / 10000 < first) continue; // если первая цифра меньше нужной пропускаем шаг
-                if (k == 0 && primes[i] / 10000 > first) return; // если первая цифра больше => прекращаем поиск
-                if (k == 4 && if_even_in_prime(primes[i])) continue; // если есть четные цифры в числе для последней строки
-                m[k] = primes[i]; // подставляем возможное значение
-                backtracking(m, k + 1, sum, first, primes, fout); // запускаем дальшейший перебор с этим значением
+                if (line == 0 && primes[i] / 10000 < first) continue; // если первая цифра меньше нужной пропускаем шаг
+                if (line == 0 && primes[i] / 10000 > first) return; // если первая цифра больше => прекращаем поиск
+                if (line == 4 && if_even_in_prime(primes[i])) continue; // если есть четные цифры в числе для последней строки
+                m[line] = primes[i]; // подставляем возможное значение
+                backtracking(m, line + 1, sum, first, primes, fout); // запускаем дальшейший перебор с этим значением
             }
     }
     else return;
@@ -197,16 +198,12 @@ void backtracking(int m[5], int k, int sum, int first, int primes[10000], FILE *
 int main() {
     FILE *fin, *fout;
     fin = fopen("input.txt", "r");
-    fout = fopen("output.txt", "w");
+    fout = fopen("output.txt", "w+");
     int sum, first;
     fscanf(fin, "%d\n%d", &sum, &first);
     int matrix[5] = { 0 };
     int primes[10000] = { 0 };
     find_suitable_primes(primes, sum);
-    // for (int i = 0; primes[i]; i++) {
-       //  printf("%d\n", primes[i]);
-    // }
-
     double time_spent = 0.0;
     clock_t begin = clock();
 
